8e252d570cf2fac76e76d808d0b2229addd8dc98
JOEL <86566234+Ludoking4@users.noreply.github.com>
2025-05-15 00:32:07 +0530

    Add files via upload

---
+++ serve_gif.py
@@ -0,0 +1,16 @@
+import http.server
+import socketserver
+import webbrowser
+import os
+
+PORT = 8000
+
+# Change working directory to where the HTML and GIF are
+os.chdir(os.path.dirname(__file__))
+
+# Start the server
+Handler = http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    webbrowser.open(f"http://localhost:{PORT}/index.html")
+    httpd.serve_forever()
